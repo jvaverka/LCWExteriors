ed99d251ab8ec23ace5f34121778289675f41dd3
Jacob Vaverka <jvaverka@protonmail.com>
2022-07-20 11:46:35 -0400

    feat(Pages): controller to route static pages for home, about, gallery, and contact

---
+++ app/views/pages/about.html.erb
@@ -0,0 +1,1 @@
+<h1 class="title">ABOUT</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :appointments
   root to: "appointments#index"
+  get "/home" => "pages#home"
+  get "/about" => "pages#about"
+  get "/contact" => "pages#contact"
+  get "/gallery" => "pages#gallery"
 end
