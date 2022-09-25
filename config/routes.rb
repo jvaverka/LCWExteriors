1d00dcd2f4ce5a14b203a6266b4ea0c2bc8b65f2
Jacob Vaverka <jvaverka@protonmail.com>
2022-09-25 19:14:08 -0400

    feat(*): simplify navigation
    Merge **about** route with **home** & merge **contact** with new form route. Remove extra tabs in navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   resources :appointments, except: [:destroy, :index, :update]
   root to: "pages#home"
   get "/home" => "pages#home"
-  get "/about" => "pages#about"
-  get "/contact" => "pages#contact"
   get "/gallery" => "pages#gallery"
 end
