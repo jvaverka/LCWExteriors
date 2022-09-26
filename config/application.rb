b1badc50744bbd7195a14ed678fd58e464e99af3
Jacob Vaverka <jvaverka@protonmail.com>
2022-09-25 20:26:42 -0400

    build(*): configure real host

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module LCWExteriors
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "lcwexteriors.com"
   end
 end
